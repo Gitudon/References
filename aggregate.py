2bc29d950f25bf2b9e8079a1a9009db0be44c5f2
Gitudon <oniku.udon.029@gmail.com>
2026-01-08 16:43:09 +0900

    edit workflow

--- aggregate.py
+++ aggregate.py
@@ -1,8 +1,33 @@
 import datetime
 
-current_time = datetime.datetime.now().strftime("%Y年%m月%d日 %H:%M:%S")
 
-HEADER = f"## 勉強になったサイトたち\n\nエンジニアとしてのスキルを磨くにあたって参考にしたサイトをまとめています。\n\n## 集計({current_time})\n\n"
+def make_header() -> str:
+    current_time = datetime.datetime.now().strftime("%Y年%m月%d日 %H:%M:%S")
+    return f"## 勉強になったサイトたち\n\nエンジニアとしてのスキルを磨くにあたって参考にしたサイトをまとめています。\n\n## 集計({current_time})\n\n"
+
+
+def aggregate_references_by_topics() -> dict:
+    aggregate_result = {}
+    current = 0
+    with open("references.md", "r", encoding="utf-8") as file:
+        for line in file:
+            if line.startswith("## "):
+                target = line[3:-1]
+                current = 0
+                aggregate_result[target] = current
+            elif line.startswith("["):
+                aggregate_result[target] += 1
+    return aggregate_result
+
+
+def make_table() -> str:
+    contents = aggregate_references_by_topics()
+    table = "| トピック | 数 |\n| :--- | ---: |\n"
+    for key, value in contents.items():
+        table += f"| {key} | {value} |\n"
+    return table[:-1]
+
 
 with open("README.md", "w") as file:
-    file.write(HEADER)
+    file.write(make_header())
+    file.write(make_table())

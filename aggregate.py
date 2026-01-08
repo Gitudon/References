78180fe34a16fb30b4a81f8f73c3de6fc1650892
Gitudon <oniku.udon.029@gmail.com>
2026-01-08 16:21:29 +0900

    use datetime

--- aggregate.py
+++ aggregate.py
@@ -1,4 +1,8 @@
-HEADER = "## 勉強になったサイトたち\n\nエンジニアとしてのスキルを磨くにあたって参考にしたサイトをまとめています。\n\n## 集計\n\n"
+import datetime
+
+current_time = datetime.datetime.now().strftime("%Y年%m月%d日 %H:%M:%S")
+
+HEADER = f"## 勉強になったサイトたち\n\nエンジニアとしてのスキルを磨くにあたって参考にしたサイトをまとめています。\n\n## 集計({current_time})\n\n"
 
 with open("README.md", "w") as file:
     file.write(HEADER)
